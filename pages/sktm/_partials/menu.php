16686b0744f091a33f0ebf4f34fea17c8ba7b791
akram30-id <afalerin2016@gmail.com>
2023-05-27 08:56:38 +0700

    fixed bug sktm & nik

--- pages/sktm/_partials/menu.php
+++ pages/sktm/_partials/menu.php
@@ -1,17 +1,10 @@
-<?php 
-// var_dump($_SESSION['user']);
-// die();
-
-?>
-
-
 <div class="panel panel-default">
   <div class="panel-body">
     <div class="row">
       <?php if ($_SESSION['user']['status_user'] !== 'Warga') { ?>
       <?php } else { ?>
         <div class="col-md-2">
-          <a href="#" class="btn btn-primary" onclick="tambah(<?= $_SESSION['user']['id_user'] ?>, <?= $_SESSION['user']['tgl_lahir_user'] ?>)">
+          <a href="#" class="btn btn-primary" onclick="tambah(<?= $_SESSION['user']['id_user'] ?>, <?= date('Y', strtotime($_SESSION['user']['tgl_lahir_user'])) ?>)">
             <i class="glyphicon glyphicon-file"></i> Tambah
           </a>
         </div>
